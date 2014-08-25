d7bcc366b0cdb5e9aac717155c75592ff4e2c396
Bartosz Zasempa <bartosz.zasempa@mail.nasza4.prv.piotrkow.pl>
2014-08-25 12:50:16 +0200

    Moved to X-Pack Services...

--- src/main/java/com/xalion95/xpackutils/reference/Reference.java
+++ src/main/java/com/xalion95/xpackutils/reference/Reference.java
@@ -8,9 +8,9 @@ public class Reference {
     public static final String CLIENT_PROXY_CLASS = "com.xalion95.xpackutils.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.xalion95.xpackutils.proxy.ServerProxy";
     public static final String GUI_FACTORY_CLASS = "com.xalion95.xpackutils.client.gui.GuiFactory";
-    public static final String UPDATE_HOSTNAME = "http://192.168.1.250";
+    public static final String UPDATE_HOSTNAME = "http://xpack.pl";
     public static final String UPDATE_FILE_1_7_10 = "/version1.7.10.txt";
-    public static final String UPDATE_FILE_JAVA = "/version.txt";
+    public static final String UPDATE_FILE_JAVA = "/javaversion.txt";
     public static final int CURRENT_VERSION = 0;
 
 }
