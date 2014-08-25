8b067f734605f663a2df5d257f735eaf47941bb4
Bartosz Zasempa <bartosz.zasempa@mail.nasza4.prv.piotrkow.pl>
2014-08-25 13:25:01 +0200

    Quick Fix

--- src/main/java/com/xalion95/xpackutils/validator/jvmArgs.java
+++ src/main/java/com/xalion95/xpackutils/validator/jvmArgs.java
@@ -19,26 +19,22 @@ public class jvmArgs {
         List<String> getFromCheck = runtimeMXBean.getInputArguments();
         String[] arguments = getFromCheck.toArray(new String[getFromCheck.size()]);
 
-        for(int counter=0;counter<arguments.length;counter++){
-            System.out.print(arguments[counter]);
-        }
-
-        /*Tokenizer = new StringTokenizer(arguments[0], "-Xmx");
+        Tokenizer = new StringTokenizer(arguments[1], "-Xmx");
         String xmxBefore = Tokenizer.nextToken();
         Tokenizer = new StringTokenizer(xmxBefore, "G");
         int xmx = Integer.parseInt(Tokenizer.nextToken());
 
-        Tokenizer = new StringTokenizer(arguments[1], "-Xms");
+        Tokenizer = new StringTokenizer(arguments[2], "-Xms");
         String xmsBefore = Tokenizer.nextToken();
         Tokenizer = new StringTokenizer(xmsBefore, "G");
         int xms = Integer.parseInt(Tokenizer.nextToken());
 
         if(xmx < 2 || xms < 1){
-            JOptionPane.showMessageDialog(null, "Nie używasz poprawnych argumentów JVM (minimalne: -Xmx2G -Xms1G). Nie możesz uruchomić paczki modów X-Pack!", "Błąd argumentów", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Nie używasz zalecanych argumentów JVM (minimalne: -Xmx2G -Xms1G) lub ich składnia jest niepoprawna. Zalecamy skopiować JVM Arguments ze strony http://xpack.pl/download.", "Błąd argumentów", JOptionPane.ERROR_MESSAGE);
             System.exit(0);
         } else {
             LogHelper.info("JVM arguments are correct...");
-        }*/
+        }
     }
 
 }
