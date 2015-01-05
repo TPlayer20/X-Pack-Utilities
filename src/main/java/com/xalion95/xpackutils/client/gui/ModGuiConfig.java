1ce2bd2f0b9abdb60e604e8479323a160db75ea3
Bartek <bartosz.zasempa@mail.nasza4.prv.piotrkow.pl>
2015-01-05 13:57:13 +0100

    Fix for unsafe or unchecked operations!

--- src/main/java/com/xalion95/xpackutils/client/gui/ModGuiConfig.java
+++ src/main/java/com/xalion95/xpackutils/client/gui/ModGuiConfig.java
@@ -3,14 +3,38 @@ package com.xalion95.xpackutils.client.gui;
 import com.xalion95.xpackutils.handler.ConfigurationHandler;
 import com.xalion95.xpackutils.reference.Reference;
 import cpw.mods.fml.client.config.GuiConfig;
+import cpw.mods.fml.client.config.IConfigElement;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.ConfigElement;
-import net.minecraftforge.common.config.Configuration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class ModGuiConfig extends GuiConfig {
     public ModGuiConfig (GuiScreen parent) {
-        super(parent,
-                new ConfigElement(ConfigurationHandler.configFile.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
-                Reference.MOD_ID, false, false, GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configFile.toString()));
+        super(parent, getProps(), Reference.MOD_ID, false, false, GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configFile.toString()));
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static List<IConfigElement> getProps()
+    {
+        Set<String> processed = new TreeSet<String>();
+        List<IConfigElement> props = new ArrayList<IConfigElement>();
+        for (String catName : ConfigurationHandler.configFile.getCategoryNames())
+        {
+            boolean shouldAdd = true;
+            for (String prevCat : processed)
+                if (catName.startsWith(prevCat))
+                {
+                    shouldAdd = false;
+                    break;
+                }
+            processed.add(catName);
+            if (shouldAdd)
+                props.add(new ConfigElement(ConfigurationHandler.configFile.getCategory(catName)));
+        }
+
+        return props;
     }
 }
