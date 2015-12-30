5990adb767b83ca3fd5698177d859cc4182a1ac3
Bartosz Zasempa <bartosz.zasempa@gmail.com>
2015-12-30 14:42:54 +0100

    New local build for 6th version of X-Pack.

--- src/main/java/com/xalion95/xpackutils/validator/OS.java
+++ src/main/java/com/xalion95/xpackutils/validator/OS.java
@@ -8,8 +8,8 @@ public class OS {
     public static void check(){
 
         LogHelper.info("Checking the system architecture...");
-        String os = System.getProperty("os.arch");
-        if (!os.equals("amd64")){
+        String os = System.getenv("PROCESSOR_ARCHITECTURE");
+        if (!os.equals("AMD64")){
             guiFrame.displayError("Nie posiadasz systemu w wersji 64-bitowej. Nie mo\u017cesz uruchomi\u0107 paczki mod\u00f3w X-Pack!", "B\u0142\u0105d systemu");
         } else {
             LogHelper.info("The system architecture is correct...");
