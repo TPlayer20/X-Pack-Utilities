fe6783a5bd240eb53b26e785fd9b501b5b37784f
barto <bartosz.zasempa@gmail.com>
2016-06-29 20:01:05 +0200

    Change version number.

--- src/main/java/com/xalion95/xpackutils/reference/Reference.java
+++ src/main/java/com/xalion95/xpackutils/reference/Reference.java
@@ -10,5 +10,5 @@ public class Reference {
     public static final String GUI_FACTORY_CLASS = "com.xalion95.xpackutils.client.gui.GuiFactory";
     public static final String UPDATE_HOSTNAME = "http://xpack.pl";
     public static final String UPDATE_FILE_1_7_10 = "/version1.7.10.txt";
-    public static final int CURRENT_VERSION = 7;
+    public static final int CURRENT_VERSION = 8;
 }
